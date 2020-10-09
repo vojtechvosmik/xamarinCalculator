013bc1835a97f0061889bde59f9dcc39295efe6e
vosmi <vosmi@DESKTOP-QA6OEV9>
2020-10-09 11:46:19 +0200

    hotofka

---
+++ calc/calc.iOS/Main.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace calc.iOS
+{
+    public class Application
+    {
+
+        static void Main(string[] args)
+        {
+            UIApplication.Main(args, null, "AppDelegate");
+            setupViews();
+        }
+
+        private static void setupViews()
+        {
+            
+        }
+
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            var button = sender as Button;
+            var text = button.Text;
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ calc/calc/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace calc
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        void NumberClicked(object sender, EventArgs args)
+        {
+            var button = sender as Button;
+            var text = button.Text;
+            this.Display.Text += text;
+        }
+
+        void ResultClicked(object sender, EventArgs args)
+        {
+            DataTable dt = new DataTable();
+            int answer = (int)dt.Compute(this.Display.Text, "");
+            this.Display.Text = answer.ToString();
+        }
+
+        void DeleteClicked(object sender, EventArgs args)
+        {
+            this.Display.Text = "";
+        }
+    }
+}
